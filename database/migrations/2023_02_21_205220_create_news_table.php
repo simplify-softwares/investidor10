2f96ceeca09f90f4070cc294302ae0a19e196e21
Gilson Reis <gilson.reis@epoc.tech>
2023-02-21 18:06:08 -0300

    * Commit inicial * Migrations das tabelas

---
+++ database/migrations/2023_02_21_205220_create_news_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("tie");
+            $table->foreignId("category_id")->constrained("category");
+            $table->mediumText("content");
+            $table->string('slug_title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
